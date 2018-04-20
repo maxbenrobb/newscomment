cdef63712802f4542515a46d4f0f997899b713cc
Max Robb <maxbenrobb@wifi_guest-10-38-115-235.uncc.edu>
2018-04-20 13:28:14 -0400

    working scrape

--- routes/api_route.js
+++ routes/api_route.js
@@ -2,6 +2,9 @@ var db = require("../models");
 var articles = require("../models/Article.js");
 var router = require("express").Router();
 
-router.get("/", articles.headlines);
+router.get("/", function(req, res) {
+    res.send(articles.headlines)
+});
+
 
 module.exports = router;
